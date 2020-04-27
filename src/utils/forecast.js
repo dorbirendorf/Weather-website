55752f29cd0d08687b6ca1b02115f2b4a846a58c
dorbirendorf <dorbiren@gmail.com>
2020-04-27 19:11:37 +0300

    more forcast info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,11 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' and UV index is: '+body.daily.data[0].uvIndex)
         }
     })
 }
+
 
 module.exports = forecast
